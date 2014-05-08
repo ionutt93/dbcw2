66c131eb8afe0522326e964901db512d3ffab3c3
Ana Dumitraș <ad12461@my.bristol.ac.uk>
2014-05-08 16:14:19 +0100

    Created game, gameCat and category.

--- create_tables.sql
+++ create_tables.sql
@@ -1,1 +1,26 @@
+CREATE TABLE game(
+    ID serial PRIMARY KEY references gameCat(gameID),
+    name varchar(255) PRIMARY KEY ,
+    description text,
+    releaseDate date NOT NULL,
+    publisher varchar(255) NOT NULL,
+    version integer,
+    avgRate integer,
+    rank integer,
+    img varchar(255),
+    website varchar(255),
+);
 
+CREATE TABLE category(
+    ID serial PRIMARY KEY references gameCat(catID),
+    name varchar(255) NOT NULL,
+    rating integer CHECK (rating >= 0 && rating <= 5)
+);
+
+CREATE TABLE gameCat(
+    ID serial PRIMARY KEY,
+    catID integer REFERENCES category(ID) ,
+    gameID integer REFERENCES game(ID),
+    rank integer,
+    UNIQUE (catID, gameID)
+);
