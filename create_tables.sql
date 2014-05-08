223ea4e599ff578b47ca777480bc91eda0665257
Liban Abdulkadir <la12808@my.bristol.ac.uk>
2014-05-08 16:39:42 +0100

    Various fixes

--- create_tables.sql
+++ create_tables.sql
@@ -1,6 +1,7 @@
-CREATE TABLE game(
-    ID serial PRIMARY KEY references gameCat(gameID),
-    name varchar(255) PRIMARY KEY ,
+DROP TABLE IF EXISTS "game", category, gameCat;
+CREATE TABLE "game" (
+    ID serial PRIMARY KEY,
+    name varchar(255),
     description text,
     releaseDate date NOT NULL,
     publisher varchar(255) NOT NULL,
@@ -8,19 +9,19 @@ CREATE TABLE game(
     avgRate integer,
     rank integer,
     img varchar(255),
-    website varchar(255),
+    website varchar(255)
 );
 
 CREATE TABLE category(
-    ID serial PRIMARY KEY references gameCat(catID),
+    ID serial PRIMARY KEY,
     name varchar(255) NOT NULL,
-    rating integer CHECK (rating >= 0 && rating <= 5)
+    rating integer CHECK (rating >= 0 AND rating <= 5)
 );
 
 CREATE TABLE gameCat(
     ID serial PRIMARY KEY,
-    catID integer REFERENCES category(ID) ,
-    gameID integer REFERENCES game(ID),
+    catID integer REFERENCES category(ID),
+    gameID integer REFERENCES "game"(ID),
     rank integer,
     UNIQUE (catID, gameID)
 );
