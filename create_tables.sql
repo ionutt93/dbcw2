c590dba5a328df3942e499343e60205922f6bef9
airimia <ai12821@my.bristol.ac.uk>
2014-05-08 16:11:46 +0100

    created tables ach, gameAch & gameOwnAch

--- create_tables.sql
+++ create_tables.sql
@@ -1,1 +1,21 @@
+CREATE TABLE ach (
+    id serial PRIMARY KEY,
+    title varchar(50) UNIQUE  
+);
 
+CREATE TABLE gameAch (
+    id serial PRIMARY KEY,
+    achId integer NOT NULL REFERENCES ach(id),
+    gameId integer NOT NULL,
+    value integer,
+    show boolean,
+    descrBefore text,
+    descrAfter text,
+    icon varchar(255)
+);
+
+CREATE TABLE gameOwnAch (
+    gameOwn integer NOT NULL REFERENCES gameOwn(id),
+    achId integer NOT NULL REFERENCES ach(id),
+    dateAchieved timestamp with time zone 
+);
