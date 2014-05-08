b90d8b2f2806184226d0dd019b6ac5871c7d7b0a
Ioan Troana <ionutt93@gmail.com>
2014-05-08 16:06:09 +0100

    created user table

--- create_tables.sql
+++ create_tables.sql
@@ -1,1 +1,14 @@
-
+DROP TABLE IF EXISTS "user";
+CREATE TABLE "user"(
+    id serial PRIMARY KEY,
+    username varchar(50) NOT NULL UNIQUE,
+    first varchar(25) NOT NULL,
+    last varchar(25) NOT NULL,
+    email varchar(50) NOT NULL UNIQUE,
+    password varchar(50) NOT NULL,
+    status text,
+    creationDate date NOT NULL,
+    lastLogin timestamp with time zone,
+    loggedIn boolean NOT NULL,
+    online boolean NOT NULL,
+    avatar varchar(200) );
