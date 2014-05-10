d7fe71057e7306a5f327d73f312ac01b27f32d4d
Liban Abdulkadir <la12808@my.bristol.ac.uk>
2014-05-10 12:46:40 +0100

    Minor fixes

--- insert_category.rb
+++ insert_category.rb
@@ -9,7 +9,7 @@ puts 'INSERT INTO "category"
 (name, rating) VALUES '
 n.times do |i|
     name = Faker::Company.name.gsub("'","\\'\\'")
-    rating = [Rubystats:NormalDistibution.new(3.5,1).rng.round,5].min
+    rating = [Rubystats::NormalDistribution.new(3.5,1).rng.round,5].min
 
     puts "('#{name}','#{rating}')" +
       (if i < (n-1) then ', ' else '' end)
