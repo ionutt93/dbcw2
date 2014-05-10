62a90981f24dcfc83da80244e97796d4edce9c88
Ioan Troana <ionutt93@gmail.com>
2014-05-10 14:57:06 +0100

    second query trigger

--- triggers.sql
+++ triggers.sql
@@ -1,13 +1,15 @@
 -- For the second question
 DROP TRIGGER IF EXISTS check_update ON gameOwn;
 CREATE TRIGGER check_update
-	AFTER UPDATE OF rating ON gameOwn
+	AFTER UPDATE OR INSERT OF rating ON gameOwn
 	FOR EACH ROW
 	EXECUTE PROCEDURE check_rating_update();
 
 CREATE OR REPLACE FUNCTION check_rating_update() RETURNS trigger AS $$
 BEGIN 
-	insert into dimitri (name,rating) values ('Dimiri',NEW.rating);
+	UPDATE game SET
+        avgRate = (SELECT AVG(rating) FROM gameOwn WHERE gameOwn.gameId = NEW.gameId)
+    WHERE id = NEW.gameId
 	RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
