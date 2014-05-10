40c43289698fadf024f3b0324db73d43f59325b2
airimia <ai12821@my.bristol.ac.uk>
2014-05-10 11:36:22 +0100

    data for friend, category & gameOwnAch

---
+++ insert_gameOwnAch.rb
@@ -0,0 +1,18 @@
+require 'faker'
+require 'digest/sha1'
+salt = 'saltysalt'
+
+n = 100
+
+date_min = Date.new(2010,01,01).to_time.to_i
+date_max = Date.new(2014,01,01).to_time.to_i
+puts 'INSERT INTO "gameOwnAch" 
+(gameOwnId, achId, dateAchieved) VALUES '
+n.times do |i|
+    gameOwnId = i + 1
+    achId = rand(30) + 1
+    dateAchieved = Time.at(rand(date_max - date_min) + date_min).strftime "%Y-%m-%d"
+    
+    puts "('#{gameOwnId}','#{achId}',\'#{dateAchieved}\'::date)" +
+      (if i < (n-1) then ', ' else '' end)
+end
