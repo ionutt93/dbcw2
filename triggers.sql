edf4571aa97b899651b38db9945535d2435ddbe2
Ioan Troana <ionutt93@gmail.com>
2014-05-10 12:27:20 +0100

    added trigger, not working

---
+++ triggers.sql
@@ -0,0 +1,13 @@
+-- For the second question
+DROP TRIGGER check_update;
+CREATE TRIGGER check_update
+	AFTER UPDATE OF rating ON "gameOwn"
+	FOR EACH ROW
+	EXECUTE PROCEDURE check_rating_update();
+
+CREATE OR REPLACE FUNCTION check_rating_update() RETURNS trigger AS $$
+BEGIN 
+	insert into dimitri (name,rating) values ('Dimiri',NEW.rating);
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
