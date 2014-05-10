63844f6da2006c50254657087662fb3e37c3debe
Liban Abdulkadir <la12808@my.bristol.ac.uk>
2014-05-10 13:48:27 +0100

    Separate SQL files

---
+++ inserts/sql/insert_category.rb.sql
@@ -0,0 +1,17 @@
+INSERT INTO "category" 
+(name, rating) VALUES 
+('Powlowski-Greenfelder','2'), 
+('Bednar, Hane and Spencer','3'), 
+('Mayert-OKonKonKon','4'), 
+('Cole-Turner','5'), 
+('Leffler, Osinski and Hilpert','4'), 
+('Sanford and Sons','3'), 
+('Schulist-Schmidt','4'), 
+('Prosacco-Kub','5'), 
+('Goyette-Daniel','3'), 
+('Schmitt-Volkman','3'), 
+('Powlowski-Considine','2'), 
+('Olson, Mueller and Friesen','3'), 
+('Schulist-Mertz','2'), 
+('Sipes-Harris','5'), 
+('Crist LLC','5')
