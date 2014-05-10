cbfb9d67cd0b2ea82e75be5114bfe1a966ecf77a
Ioan Troana <ionutt93@gmail.com>
2014-05-10 15:46:53 +0100

    modified query

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 -- 1.Given a game, list all the users who own that game 
 SELECT "user".username FROM "user", game, "gameOwn" WHERE 
-	game.name = (SELECT game.name FROM game WHERE game.id = ((random() * 99) + 1)) AND
-	game.id = "gameOwn".gameID AND
-	"gameOwn".userID = user.ID
+    game.id = round((random() * 99) + 1) AND
+    game.id = "gameOwn".gameId AND
+    "gameOwn".userId = "user".id;
 
 -- 2.Automatically update a game’s average rating whenever a user adds or updates their rating for that game
