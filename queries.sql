e339e9f205e6320a5abb15e8c8ae3284a261681d
Liban Abdulkadir <la12808@my.bristol.ac.uk>
2014-05-10 13:18:00 +0100

    New directory for scripts

--- queries.sql
+++
@@ -1,7 +0,0 @@
--- 1.Given a game, list all the users who own that game 
-SELECT "user".username FROM "user", game, "gameOwn" WHERE 
-	game.name = (SELECT game.name FROM game WHERE game.id = ((random() * 99) + 1)) AND
-	game.id = "gameOwn".gameID AND
-	"gameOwn".userID = user.ID
-
--- 2.Automatically update a game’s average rating whenever a user adds or updates their rating for that game
