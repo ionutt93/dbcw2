5a29cc2049bc09e119bb875dec5a2f70e9113145
Liban Abdulkadir <la12808@my.bristol.ac.uk>
2014-05-18 14:45:09 +0100

    Changed q4 function name

--- functions.sql
+++ functions.sql
@@ -11,8 +11,8 @@ END
 $$ LANGUAGE plpgsql;
 
 -- Question 4
-CREATE OR REPLACE FUNCTION q4(IN username varchar(255), IN gamename character varying) 
-RETURNS table(op varchar(255)) AS $$ 
+CREATE OR REPLACE FUNCTION my_game_status(IN username varchar(255), IN gamename character varying) 
+RETURNS character varying AS $$ 
 DECLARE
     op character varying;
     totalu int;
@@ -21,22 +21,21 @@ DECLARE
     pc int;
     pctext character varying;
 BEGIN 
-    totalu := count(highscore) FROM gameOwn JOIN game ON game.name = q4.gamename 
+    totalu := count(highscore) FROM gameOwn JOIN game ON game.name = my_game_status.gamename 
         AND game.id = gameOwn.gameid;
     SELECT INTO ranku,highscoreu sq.rank,sq.highscore FROM
     (SELECT row_number() OVER (ORDER BY highscore DESC) AS rank,
         highscore,u.username FROM gameOwn
     JOIN "user" u ON u.id = gameOwn.userid
-    JOIN game ON game.id = gameown.gameid AND game.name = q4.gamename) AS sq 
-    WHERE sq.username = q4.username;
+    JOIN game ON game.id = gameown.gameid AND game.name = my_game_status.gamename) AS sq 
+    WHERE sq.username = my_game_status.username;
     pc := 100*ranku/CAST(totalu AS double precision);
     pctext := 'Top';
     IF pc > 50 THEN
         pc := 100 - pc;
         pctext := 'Bottom';
     END IF;
-    q4.op := format('%s points - %s (%s %s%%)',highscoreu,ranku,pctext,pc);
-    RETURN next;
+    RETURN format('%s points - %s (%s %s%%)',highscoreu,ranku,pctext,pc);
 END;
 $$ LANGUAGE plpgsql;
 
